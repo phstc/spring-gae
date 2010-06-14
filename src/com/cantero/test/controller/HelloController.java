ef5ad9dd25b3ff99431b841d414dbea1b6db436d
Pablo <pablohstc@gmail.com>
2010-06-14 00:49:19 -0300

    First commit

---
+++ src/com/cantero/test/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.cantero.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/helloWorld")
+	public ModelAndView helloWorld() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("helloWorld");
+		mav.addObject("message", "Hello Spring 3 RC1 Maven World!");
+		return mav;
+
+	}
+
+}
